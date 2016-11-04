c0db194a84d4b3c382d9da8d912c41e830ffdc82
Ivan Malison <IvanMalison@gmail.com>
2016-11-04 04:08:49 -0700

    Update commentary section

--- tile.el
+++ tile.el
@@ -23,11 +23,8 @@
 
 ;;; Commentary:
 
-;; multi-line aims to provide a flexible framework for automatically
-;; multi-lining and single-lining function invocations and definitions,
-;; array and map literals and more. It relies on functions that are
-;; defined on a per major mode basis wherever it can so that it operates
-;; correctly across many different programming languages.
+;; tile provides a way to interactively cycle through pre-defined custom layout
+;; definitions à la XMonad.
 
 ;;; Code:
 
